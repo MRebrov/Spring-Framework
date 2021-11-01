866b64bd2e167f182b4726079cdfa905e95f65c4
Rebrov <mrebrov@t-systems.ru>
2021-11-01 15:50:41 +0300

    initial commit

---
+++ src/main/java/org/example/app/services/BookService.java
@@ -0,0 +1,47 @@
+package org.example.app.services;
+
+import org.example.web.dto.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import org.apache.log4j.Logger;
+
+@Service
+public class BookService {
+    private final ProjectRepository<Book> bookRepo;
+    private final Logger logger = Logger.getLogger(BookService.class);
+
+    @Autowired
+    public BookService(org.example.app.services.ProjectRepository<Book> bookRepo) {
+        this.bookRepo = bookRepo;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepo.retreiveAll();
+    }
+
+    public void saveBook(Book book) {
+        bookRepo.store(book);
+    }
+
+    public boolean removeBookById(Integer bookIdToRemove) {
+        return bookRepo.removeItemById(bookIdToRemove);
+    }
+
+    private void defaultInit() {
+        logger.info("default INIT in book service");
+    }
+
+    private void defaultDestroy() {
+        logger.info("default DESTROY in book service");
+    }
+
+    public void removeByRegEx(String regex) {
+        getAllBooks().forEach(book -> {
+            if (book.getAuthor().matches(regex) || book.getTitle().matches(regex) || book.getSize().toString().matches(regex)) {
+                bookRepo.removeItemById(book.getId());
+            }
+        });
+    }
+}
